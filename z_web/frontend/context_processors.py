c184e85934940dab293be7af9c97193bcf96d5c7
Matias Varela <matu.varela@gmail.com>
2017-11-12 15:03:14 -0300

    no armar menu para vistas sin autenticación

--- z_web/frontend/context_processors.py
+++ z_web/frontend/context_processors.py
@@ -4,12 +4,14 @@ from zweb_utils.views import generate_menu_user
 
 
 def user_menu(request):
-    # warning: si se cambia la key, actualizar el invalidador en logout
-    key = "user_menu_{}".format(request.user.pk)
-    if cache.get(key):
-        return cache.get(key)
+    if request.user.is_authenticated:
+        # warning: si se cambia la key, actualizar el invalidador en logout
+        key = "user_menu_{}".format(request.user.pk)
+        if cache.get(key):
+            return cache.get(key)
+        else:
+            menu = {'user_menu': generate_menu_user(request.user)}
+            cache.set(key, menu, 60 * 60 )  # cached for 1 hours
+            return menu
     else:
-        menu = {'user_menu': generate_menu_user(request.user)}
-        cache.set(key, menu, 60 * 60 )  # cached for 1 hours
-        return menu
-
+        return {}
